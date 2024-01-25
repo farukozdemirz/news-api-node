1557534e0a3d9874eec714a2038f037cf678dedd
farukozdemirz <faruk@jotform.com>
2024-01-25 23:04:26 +0300

    added mongodb url

--- routes/api/news.js
+++ routes/api/news.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const NewsController = require('../../controllers/newsController');
 
-router.post('/news', NewsController.createNews);
+router.post('/create', NewsController.createNews);
 
-router.get('/news', NewsController.getAllNews);
+router.get('/getAll', NewsController.getAllNews);
 
 router.get('/news/category/:category', NewsController.getNewsByCategory);
 
